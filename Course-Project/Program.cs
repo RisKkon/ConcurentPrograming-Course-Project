cf5183135416725e35c5263613faa896979f379b
RisKkon <kaboobs00@gmai.com>
2020-12-03 16:25:53 +0200

    initial commit

--- Course-Project/Program.cs
+++ Course-Project/Program.cs
@@ -6,9 +6,9 @@ namespace Course_Project
         static void Main(string[] args)
         {
             var area51 = new Base();
-            var agent1 = new Agent("Gosho", 1);
-            var agent2 = new Agent("MaikulSKavala", 2);
-            var agent3 = new Agent("MishoShamara", 3);
+            var agent1 = new Agent("Mike", 1);
+            var agent2 = new Agent("John", 2);
+            var agent3 = new Agent("Monika", 3);
             var elevator = new Elevator();
             area51.ListForAgentsWaitingToEnterBase.Add(agent1);
             area51.ListForAgentsWaitingToEnterBase.Add(agent2);
